7ea5b9f6c35468e647f6fb4cae139cfcadd813b3
otabek-kholmirzaev <kholmirzaevotabek1@gmail.com>
2026-04-06 18:57:39 +0500

    Add Singleton implementation with Lazy Loading

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,11 +1,28 @@
-﻿var singleton1 = Singleton.Instance;
+﻿Console.WriteLine("Before accessing instance");
+
+var singleton1 = Singleton.Instance;
+
+Console.WriteLine("After accessing instance");
+
 var singleton2 = Singleton.Instance;
 
 sealed class Singleton
 {
-    public static Singleton Instance { get; } = new();
+    private static Singleton _instance = default!;
+    public static Singleton Instance { 
+        get
+        {
+            if (_instance is null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    } 
 
     private Singleton()
     {
+        Console.WriteLine("Instantiating singleton");
     }
 }
